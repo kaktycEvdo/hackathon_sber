2537cf9a40bcc1c4b9009872267a23b72de25812
kaktycEvdo <ilya.tretyakov.2005@bk.ru>
2023-11-25 17:22:31 +0700

    message yeah

---
+++ sber/src/App.jsx
@@ -0,0 +1,23 @@
+import { useState } from 'react'
+import sberLogo from './assets/sber_logo_full.svg'
+import Header from './components/UI/Header/Header'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import {faUser} from '@fortawesome/free-solid-svg-icons'
+import Main from './components/Main'
+
+function App() {
+	return(
+		<>
+		<Header
+		logo={<img src={sberLogo}/>}
+		lk_href = {"lk/"}
+		lk_icon = {<FontAwesomeIcon icon={faUser}/>}
+		/>
+		<main>
+			<Main logo = {<img src={sberLogo}/>} lk_href = {"lk/"}/>
+		</main>
+		</>
+	)
+}
+
+export default App
---
+++ sber/src/components/Main.jsx
@@ -0,0 +1,63 @@
+import Modal from './UI/Modal/Modal'
+import { Component } from 'react';
+import { faDownload } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+class Main extends Component {
+    state = { opened: false }
+
+    data = [
+        {'ФИО': 'Фамилич'+' '+'Имён'+' '+'Отчествович', 'dtype' : 'text'},
+        {'Дата рождения': '25.11.1999', 'dtype' : 'date'},
+        {'Серия № паспорта': '2555 251444', 'dtype' : 'date'},
+        {'Адрес регистрации': 'Россия г. Новосибирск ул. Коммунистическая д. 32', 'dtype' : 'text'},
+        {'Адрес проживания': 'Россия г. Новосибирск ул. Коммунистическая д. 32', 'dtype' : 'text'},
+        {'Семейное положение': 'Холост', 'dtype' : 'text'},
+        {'Наличие детей': 0, 'dtype' : 'number'},
+        {'Должность': 'Работник общественного питания', 'dtype' : 'text'},
+        {'Ежемесячный доход по месту работы': 23000, 'dtype' : 'number'},
+        {'Документ, подтверждающий доход': 'типа документ ага', 'dtype' : 'file'},
+        {'Ежемесячный дополнительный доход': 0, 'dtype' : 'text'},
+        {'Документ, подтверждающий дополнительный доход': 'типа его нет ага', 'dtype' : 'file'},
+        {'Место работы': 'Вкусно - и Точка!', 'dtype' : 'text'},
+        {'Стаж работы': 3, 'dtype' : 'number'},
+        {'Источник доп. дохода': 'Нет', 'dtype' : 'text'},
+        {'Наличие сбережений на счёте в банке': 'Нет', 'dtype' : 'text'}
+    ]
+
+    lk_data = {
+        'ФИО': 'Фамилич'+' '+'Имён'+' '+'Отчествович',
+        'Домен': 'somedomain@sber.org',
+        'Электронная почта': 'email@mail.ru',
+        'Пароль': 'parolparol'
+    }
+    
+    render() { 
+        return (
+            <>
+                <Modal opened={this.state.opened} close={() => this.setState({opened: false})}>a</Modal>
+                
+                <div className='main_form'>
+                    <div className='form_logo'>{this.props.logo}</div>
+                    {this.data.map((object, index) => {
+                        return <div key={index}>
+                                    <div className={object['dtype'] === 'file' ? "file_input" : null}>
+                                        <label htmlFor={object['dtype']+index}>
+                                            {Object.keys(object)[0]}
+                                            {object['dtype'] === 'file' ? <FontAwesomeIcon icon={faDownload}/> : null}
+                                        </label>
+                                    </div>
+                                    <br/>
+                                    <input id={object['dtype']+index} hidden={object['dtype'] === 'file' ? true : false} type={object['dtype']} value={object[0]}/>
+                                </div>
+                    })}
+                </div>
+                <menu>
+                    <button onClick={() => this.setState({opened: true})}>Сезам откройся</button>
+                </menu>
+            </>
+        );
+    }
+}
+ 
+export default Main;
